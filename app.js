0873bf8551d4c0cf1f2d8ac49035ae156c148f4c
danny amit <danny@iamit.org>
2025-08-08 08:50:05 -0400

    added the level up's

--- app.js
+++ app.js
@@ -209,6 +209,77 @@ function basicCalculator(firstParameter, secondParameter, operator) {
     return everyOperator[operator](firstParameter, secondParameter)
 }
 
+console.log('Exercise 9 Result:', basicCalculator(2, 10, "divide"));
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade(scoreInput) {
+    if (scoreInput >= 90) {
+        return "A";
+    } else if (scoreInput >= 80) {
+        return "B";
+    } else if (scoreInput >= 70) {
+        return "C";
+    } else if (scoreInput >= 60) {
+        return "D";
+    }else return "F"
+}
 
 
-console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+console.log('Exercise 10 Result:', calculateGrade(50));
+
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+function createUsername(firstName, lastName) {
+    let firstThreeOfFirst = firstName.slice(0, 3);
+    let firstThreeOfSecond = lastName.slice(0, 3);
+    let totalCharactersNum = firstName.length + lastName.length;
+    let userName = firstThreeOfFirst + firstThreeOfSecond + totalCharactersNum;
+    return userName
+}
+
+console.log('Exercise 11 Result:', createUsername("Danny", "Amit"));
+
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+let numArgs = function() {
+    return arguments.length;
+}
+
+
+console.log('Exercise 12 Result:', numArgs(1, 3, 4));
